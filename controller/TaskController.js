376933d83045924d78648f513cdcb1289d141b8e
Alex Gomes <123321801+AlexcrGomes@users.noreply.github.com>
2023-11-28 20:33:46 +0000

    Update 3 files

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,255 +1,256 @@
-const Task = require('../models/Task');
-const User = require('../models/User');
-const passport = require('passport');
+const Task = require("../models/Task");
+const User = require("../models/User");
 
-
-let message = '';
-let type = '';
+let message = "";
+let type = "";
 
 const getALLTasks = async (req, res) => {
-    try {
-        setTimeout(() => {
-            message = ""
-        }, 1000);
-        const tasksList = await Task.find();
-        return res.render('index', {
-            tasksList,
-            task: null,
-            taskDelete: null,
-            message,
-            type
-        });
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
+    const tasksList = await Task.find();
+    return res.render("index", {
+      tasksList,
+      task: null,
+      taskDelete: null,
+      message,
+      type,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const createTask = async (req, res) => {
-    const task = req.body;
+  const task = req.body;
 
-    if (!task.task) {
-        message = 'Please enter a task'
-        type = 'danger'
-        return res.redirect('/');
-    }
+  if (!task.task) {
+    message = "Please enter a task";
+    type = "danger";
+    return res.redirect("/");
+  }
 
-    try {
-        await Task.create(task);
-        message = 'Task created successfully'
-        type = 'success'
-        return res.redirect('/');
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    await Task.create(task);
+    message = "Task created successfully";
+    type = "success";
+    return res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const getById = async (req, res) => {
-    try {
-        const tasksList = await Task.find();
-        if (req.params.method == 'update') {
-            const task = await Task.findOne({_id: req.params.id});
-            res.render('index', {
-                task,
-                taskDelete: null,
-                tasksList,
-                message,
-                type
-            });
-        } else {
-            const taskDelete = await Task.findOne({_id: req.params.id});
-            res.render('index', {
-                task: null,
-                taskDelete,
-                tasksList,
-                message,
-                type
-            });
-        }
-
-    }catch (err) {
-        res.status(500).send({error: err.message});
+  try {
+    const tasksList = await Task.find();
+    if (req.params.method == "update") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", {
+        task,
+        taskDelete: null,
+        tasksList,
+        message,
+        type,
+      });
+    } else {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", {
+        task: null,
+        taskDelete,
+        tasksList,
+        message,
+        type,
+      });
     }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const updateOneTask = async (req, res) => {
-    try {
-        const task = req.body;
-        await Task.updateOne({ _id: req.params.id}, task);
-        message = 'Task updated successfully'
-        type = 'success'
-        res.redirect('/');
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    message = "Task updated successfully";
+    type = "success";
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const deleteOneTask = async (req, res) => {
-    try {
-        await Task.deleteOne({ _id: req.params.id});
-        message = 'Task deleted successfully'
-        type = 'success'
-        res.redirect('/');
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    message = "Task deleted successfully";
+    type = "success";
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const taskCheck = async (req, res) => {
-    try {
-        const task = await Task.findOne({ _id: req.params.id });
-        task.check ? task.check = false : task.check = true;
-        await Task.updateOne({ _id: req.params.id }, task);
-        res.redirect("/");
-    } catch (err) {
-        res.status(500).send({ error: err.message });
-    }
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    task.check ? (task.check = false) : (task.check = true);
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const signup = async (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    if (!user.nome || !user.email || !user.senha) {
-        message = 'Todos os campos são obrigatórios';
-        type = 'danger';
-        return res.redirect('/cadastro');
-    }
+  if (!user.nome || !user.email || !user.senha) {
+    message = "Todos os campos são obrigatórios";
+    type = "danger";
+    return res.redirect("/cadastro");
+  }
 
-    const existingUser = await User.findOne({ email: user.email });
-    if (existingUser) {
-        message = 'Já existe um usuário cadastrado com esse e-mail';
-        type = 'danger';
-        return res.redirect('/cadastro');
-    }
+  const existingUser = await User.findOne({ email: user.email });
+  if (existingUser) {
+    message = "Já existe um usuário cadastrado com esse e-mail";
+    type = "danger";
+    return res.redirect("/cadastro");
+  }
 
-    try {
-        await User.create(user);
-        message = 'Usuário cadastrado com sucesso';
-        type = 'sucess';
-        return res.redirect('/login');
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    await User.create(user);
+    message = "Usuário cadastrado com sucesso";
+    type = "sucess";
+    return res.redirect("/login");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const signin = async (req, res) => {
+  try {
     const { email, senha } = req.body;
 
     if (!email || !senha) {
-        message = 'Por favor, preencha os campos são obrigatórios';
-        type = 'danger';
-        return res.redirect('/');
+      return res
+        .status(400)
+        .redirect("/login")
+        .send("Por favor, preencha todos os campos obrigatórios.");
     }
 
-    try {
-        const existingUser = await User.findOne({ email });
-        if (!existingUser) {
-            message = 'E-mail não cadastrado, por favor realize o cadastro';
-            type = 'danger';
-            return res.redirect('/');
-        }
-        if (senha !== existingUser.senha) {
-            message = 'Senha ou e-mail incorreto';
-            type = 'danger';
-            return res.redirect('/');
-        }
-        message = `Bem vindo, ${existingUser.nome.split(' ')[0]}`;
-        type = 'success';
-        return passport.authenticate('local', {
-            successRedirect: '/telaadm',
-            failureRedirect: '/',
-            failureFlash: true
-        })(req, res, '/telaadm');
-    }catch (err) {
-        res.status(500).send({error: err.message});
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      console.log("Usuário não cadastrado");
+      return res
+        .status(401)
+        .redirect("/login")
+        .send("E-mail não cadastrado. Por favor, realize o cadastro.");
     }
+
+    const isPasswordValid = await existingUser.comparePassword(senha);
+
+    if (!isPasswordValid) {
+      console.log("Senha incorreta");
+      return res
+        .status(401)
+        .redirect("/login")
+        .send("Senha ou e-mail incorretos.");
+    }
+
+    console.log("Login bem-sucedido");
+    return res.redirect(`/${existingUser._id}/telaadm`);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ error: "Erro interno do servidor." });
+  }
 };
 
 const logout = (req, res) => {
-    req.logout();
-    res.redirect('/');
+  res.redirect("/");
 };
 
 const getALLUsers = async (req, res) => {
-    try {
-        setTimeout(() => {
-            message = ""
-        }, 1000);
-        const usersList = await User.find();
-        return res.render('/telaadm', {
-            usersList,
-            user: null,
-            userDelete: null,
-            message,
-            type
-        });
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
+    const usersList = await User.find();
+    return res.render("/telaadm", {
+      usersList,
+      user: null,
+      userDelete: null,
+      message,
+      type,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const updateOneUser = async (req, res) => {
-    try {
-        const user = req.body;
-        await user.updateOne({ _id: req.params.id}, user);
-        message = 'Usuario atualizado com sucesso'
-        type = 'success'
-        res.redirect('/telaadm');
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    const user = req.body;
+    await user.updateOne({ _id: req.params.id }, user);
+    message = "Usuario atualizado com sucesso";
+    type = "success";
+    res.redirect("/telaadm");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const deleteOneUser = async (req, res) => {
-    try {
-        await User.deleteOne({ _id: req.params.id});
-        message = 'Usuario deletado com sucesso'
-        type = 'success'
-        res.redirect('/telaadm');
-    }catch (err) {
-        res.status(500).send({error: err.message});
-    }
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    message = "Usuario deletado com sucesso";
+    type = "success";
+    res.redirect("/telaadm");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 const getByIdUser = async (req, res) => {
-    try {
-        const usersList = await User.find();
-        if (req.params.method == 'update') {
-            const user = await User.findOne({_id: req.params.id});
-            res.render('/telaadm', {
-                user,
-                userDelete: null,
-                usersList,
-                message,
-                type
-            });
-        } else {
-            const taskDelete = await User.findOne({_id: req.params.id});
-            res.render('/telaadm', {
-                user: null,
-                userDelete,
-                usersList,
-                message,
-                type
-            });
-        }
-
-    }catch (err) {
-        res.status(500).send({error: err.message});
+  try {
+    const usersList = await User.find();
+    if (req.params.method == "update") {
+      const user = await User.findOne({ _id: req.params.id });
+      res.render("/telaadm", {
+        user,
+        userDelete: null,
+        usersList,
+        message,
+        type,
+      });
+    } else {
+      const taskDelete = await User.findOne({ _id: req.params.id });
+      res.render("/telaadm", {
+        user: null,
+        userDelete,
+        usersList,
+        message,
+        type,
+      });
     }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
 };
 
 module.exports = {
-    getALLTasks,
-    createTask,
-    getById,
-    updateOneTask,
-    deleteOneTask,
-    taskCheck,
-    signup,
-    signin,
-    logout,
-    getALLUsers,
-    updateOneUser,
-    deleteOneUser,
-    getByIdUser,
+  getALLTasks,
+  createTask,
+  getById,
+  updateOneTask,
+  deleteOneTask,
+  taskCheck,
+  signup,
+  signin,
+  logout,
+  getALLUsers,
+  updateOneUser,
+  deleteOneUser,
+  getByIdUser,
 };
--- index.js
+++ index.js
@@ -1,86 +1,36 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const routes = require('./routes/routes');
-const connectToDb = require('./database/db');
-const port = 3000
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+const express = require("express");
+const path = require("path");
+const connectToDb = require("./database/db");
+const routes = require("./routes/routes");
 
 connectToDb();
 
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
+const port = 3000;
+
+// Configurações do Express
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: 'secretpassword',
-    resave: false,
-    saveUninitialized: false
-}));
-const users  = [
-    { _id: 1, email: 'email', senha: 'senha' }
-];
-passport.use(new LocalStrategy(
-    (email, senha, done) => {
-        const user = users.find(u => u.email  === email && u.senha === senha);
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false, { message: 'Credenciais inválidas.' });
-        }
-    }
-));
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-passport.deserializeUser((id, done) => {
-    const user = users.find(u => u.id === id);
-    done(null, user);
-});
-const isAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
-};
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/pagina-autenticada',
-    failureRedirect: '/login',
-    failureFlash: true
-}));
-app.get('/telaadm', isAuthenticated, (req, res) => {
-    res.render(__dirname + "/src/telaadm.ejs", {
-        message: '',
-        type: ''
-    });
-});
-app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/login');
-});
 
-
-
+// Rotas
 
 app.use(routes);
 
-
-
-app.get("/login", function(req, res) {
-    res.render(__dirname + "/src/login.ejs", {
-        message: '',
-        type: ''
-    });
+app.get("/login", (req, res) => {
+  res.render(path.join(__dirname, "/src/login.ejs"), {
+    message: "",
+    type: "",
+  });
 });
 
-app.get("/cadastro", function(req, res) {
-    res.render(__dirname + "/src/cadastro.ejs", {
-        message: '',
-        type: ''
-    });
+app.get("/cadastro", (req, res) => {
+  res.render(path.join(__dirname, "/src/cadastro.ejs"), {
+    message: "",
+    type: "",
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
